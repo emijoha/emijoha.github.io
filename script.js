dbdcb182c9172b62bfa91ddcdf2449bfd2cde904
emi <ejhartline@gmail.com>
2020-04-02 16:24:29 -0400

    added event listeners to nav buttons

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
+// DOM elements
+var projectsBtn = $("#projects");
+var aboutBtn = $("#about");
+var contactBtn = $("#contact");
+var projectContent = $("#projectContent");
+var aboutContent = $("#aboutContent");
+var contactContent = $("#contactContent")
 
+projectsBtn.click( function () {
+    if (projectContent.hasClass("is-hidden")) {
+        projectContent.removeClass("is-hidden");
+    }
+    else {
+        projectContent.addClass("is-hidden");
+    }
+});
+
+aboutBtn.click( function () {
+    if (aboutContent.hasClass("is-hidden")) {
+        aboutContent.removeClass("is-hidden");
+    }
+    else {
+        aboutContent.addClass("is-hidden");
+    }
+})
+
+contactBtn.click( function () {
+    if (contactContent.hasClass("is-hidden")) {
+        contactContent.removeClass("is-hidden");
+    }
+    else {
+        contactContent.addClass("is-hidden");
+    }
+})
